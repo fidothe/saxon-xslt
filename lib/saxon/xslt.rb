3c0d721cb951909a2e49bdc3143c6769a154d201
Matt Patterson <matt@reprocessed.org>
2013-09-17 17:46:54 +0100

    Refactor and spec additions * Accept what Nokogiri accepts for Xslt.compile equivalent * Add specs! * Emit XdmNode-using result rather than string from Xslt#transform

--- lib/saxon/xslt.rb
+++ lib/saxon/xslt.rb
@@ -8,6 +8,7 @@ java_import javax.xml.transform.stream.StreamSource
 module Saxon
   module S9API
     java_import 'net.sf.saxon.s9api.Processor'
+    java_import 'net.sf.saxon.s9api.XdmDestination'
   end
 
   class Xslt
@@ -15,25 +16,31 @@ module Saxon
       new(xslt_path)
     end
 
-    def initialize(xslt_path)
+    def initialize(xslt_path_or_io)
       @processor = S9API::Processor.new(false)
       @compiler = @processor.newXsltCompiler()
-      @xslt = @compiler.compile(StreamSource.new(java.io.File.new(xslt_path)))
+      @xslt = @compiler.compile(to_stream_source(xslt_path_or_io))
     end
 
     def transform(xml_path_or_io)
-      unless xml_path_or_io.respond_to?(:read)
-        xml_path_or_io = File.open(xml_path_or_io, 'r')
-      end
-      serializer = @processor.newSerializer()
-      output = java.io.StringWriter.new()
-      serializer.setOutputWriter(output)
-      xml = @processor.newDocumentBuilder().build(StreamSource.new(xml_path_or_io.to_inputstream))
+      output = S9API::XdmDestination.new
+      xml = @processor.newDocumentBuilder().build(to_stream_source(xml_path_or_io))
       transformer = @xslt.load
       transformer.setInitialContextNode(xml)
-      transformer.setDestination(serializer)
+      transformer.setDestination(output)
       transformer.transform
-      output.toString
+      output.getXdmNode
+    end
+
+    private
+
+    def to_stream_source(path_io_or_string)
+      StreamSource.new(to_inputstream(path_io_or_string))
+    end
+
+    def to_inputstream(path_io_or_string)
+      return path_io_or_string.to_inputstream if path_io_or_string.respond_to?(:read)
+      return java.io.StringReader.new(path_io_or_string)
     end
   end
 end
