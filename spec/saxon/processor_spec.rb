10b933a7356ea628bf1e8640fe0f2c57d8f98d84
Matt Patterson <matt@reprocessed.org>
2014-08-10 22:49:51 +0200

    Allow configuration file to be passed in
    This tackles the main part of @papile's #1.
    
    The test for this is messy because it's unclear to me whether the
    configuration object is something that should be manipulated after the
    processor has been created. Feedback from @michaelhkay @ond1 or @papile
    appreciated on that one.

--- spec/saxon/processor_spec.rb
+++ spec/saxon/processor_spec.rb
@@ -2,14 +2,27 @@ require 'spec_helper'
 require 'saxon/processor'
 
 describe Saxon::Processor do
-  let(:processor) { Saxon::Processor.new }
   let(:xsl_file) { File.open(fixture_path('eg.xsl')) }
 
-  it "can make a new XSLT instance" do
-    expect(processor.XSLT(xsl_file)).to respond_to(:transform)
+  context "without configuration" do
+    let(:processor) { Saxon::Processor.new }
+
+    it "can make a new XSLT instance" do
+      expect(processor.XSLT(xsl_file)).to respond_to(:transform)
+    end
+
+    it "can make a new XML instance" do
+      expect(processor.XML(xsl_file)).to respond_to(:node_kind)
+    end
   end
 
-  it "can make a new XML instance" do
-    expect(processor.XML(xsl_file)).to respond_to(:node_kind)
+  context "with a configuration file" do
+    it "works, given a valid config XML file" do
+      processor = Saxon::Processor.new(File.open(fixture_path('config.xml')))
+
+      saxon_processor = processor.instance_variable_get(:@processor)
+      configuration = saxon_processor.underlying_configuration
+      expect(configuration.xml_version).to eq(11)
+    end
   end
 end
